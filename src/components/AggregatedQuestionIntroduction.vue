9a6e8e7e2d9dbd353d3a0cbb382c0a07adf6e358
samul-1 <bsamuele00@gmail.com>
2021-04-26 12:24:53 +0200

    added code highlight to aggregated question introduction

--- src/components/AggregatedQuestionIntroduction.vue
+++ src/components/AggregatedQuestionIntroduction.vue
@@ -1,14 +1,19 @@
 <template>
   <div class="p-2 mb-2 bg-gray-300 rounded-md">
     <div v-html="text"></div>
+    <div v-highlight v-html="highlightCode(text)"></div>
   </div>
 </template>
 
 <script>
+import { highlightCode } from '../constants.js'
+
 export default {
   name: 'AggregatedQuestionIntroduction',
-  props: ['text']
-  // TODO import the code highlight function and use it on the text
+  props: ['text'],
+  methods: {
+    highlightCode
+  }
 }
 </script>
 
