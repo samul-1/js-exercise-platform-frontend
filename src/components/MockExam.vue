06a07e684954f1c48159236c1f4058777b3a6e76
samul-1 <bsamuele00@gmail.com>
2021-06-18 18:47:44 +0200

    reduced content in pdf exam simulation to temporarily work around the max height limit

--- src/components/MockExam.vue
+++ src/components/MockExam.vue
@@ -1,15 +1,15 @@
 <template>
-  <div class="p-10">
-    <h1 v-if="data.questions.length" class="text-3xl font-semibold">
+  <div class="px-10">
+    <!--<h1 v-if="data.questions.length" class="text-3xl font-semibold">
       Simulazione domande
     </h1>
     <p class="mt-2">
       <i class="text-yellow-700 fas fa-exclamation-circle"></i> Nota: anche se
       le domande sono mostrate tutte insieme in questo documento, ricorda che
       allo studente verranno mostrate sequenzialmente, una alla volta.
-    </p>
+    </p>-->
     <div
-      class="py-8 my-8 border-b border-gray-300 last:border-b-0"
+      class="py-2 my-2 border-b border-gray-300 last:border-b-0 atomic-box"
       v-for="(question, index) in data.questions"
       :key="'q-' + question.id"
     >
@@ -20,20 +20,20 @@
         :text="question.introduction"
       ></AggregatedQuestionIntroduction>
       <Question :question="question" :preview="true"></Question>
-      <div class="html2pdf__page-break"></div>
+      <!--<div class="html2pdf__page-break"></div>-->
     </div>
     <!-- // todo make an actual exercise preview component -->
     <h1 v-if="data.exercises.length" class="text-3xl font-semibold">
       Simulazione esercizi
     </h1>
     <div
-      class="py-2 my-2 border-b border-gray-300 last:border-b-0"
+      class="py-2 my-2 border-b border-gray-300 last:border-b-0 atomic-box"
       v-for="(exercise, index) in data.exercises"
       :key="'e-' + exercise.id"
     >
       <h1 class="text-2xl font-medium">Esercizio {{ index + 1 }}</h1>
       <p v-html="highlightCode(exercise.text)"></p>
-      <div class="html2pdf__page-break"></div>
+      <!--<div class="html2pdf__page-break"></div>-->
     </div>
   </div>
 </template>
@@ -59,4 +59,8 @@ export default {
 }
 </script>
 
-<style></style>
+<style>
+.atomic-box {
+  break-inside: avoid;
+}
+</style>
