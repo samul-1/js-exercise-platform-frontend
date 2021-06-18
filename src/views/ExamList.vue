a5a326e29075fe72a114c9bd4fb9ece06f23df8d
samul-1 <bsamuele00@gmail.com>
2021-06-18 18:04:06 +0200

    added distinction between right and wrong answers in exam pdf preview

--- src/views/ExamList.vue
+++ src/views/ExamList.vue
@@ -139,7 +139,7 @@
       :show-layout="false"
       :float-layout="true"
       :enable-download="true"
-      :preview-modal="true"
+      :preview-modal="false"
       :paginate-elements-by-height="1400"
       filename="Simulazione"
       :pdf-quality="2"
